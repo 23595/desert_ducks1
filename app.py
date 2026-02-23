04662003e4d7a76eea9c39185bb51ecfd980350c
23595 <23595@burnside.school.nz>
2026-02-24 11:51:33 +1300

    Working on navigation and layout

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, g, render_template
 import sqlite3
+import random
 
 DATABASE = 'ducks.db'
 
@@ -32,10 +33,14 @@ def home():
 
 @app.route('/setting_select')
 def setting_select():
-    #Home page
+    sql = 'SELECT COUNT(setting_id) FROM settings'
+    result = query_db(sql)
+    result = result[0]
+    settingcount = int(result[0])
+    random_setting = random.randint(1, settingcount)
     sql = 'SELECT setting_name, setting_id FROM settings'
     result = query_db(sql)
-    return render_template("setting_select.html",result=result)
+    return render_template("setting_select.html",result=result,random_setting=random_setting)
 
 @app.route('/setting/<int:id>')
 def setting(id):
@@ -43,14 +48,14 @@ def setting(id):
     sql = """SELECT setting_name, setting_desc FROM settings
     WHERE setting_id = ?;"""
     result = query_db(sql, (id,), True)
-    return str(result)
+    return render_template("setting_desc.html",result=result)
 
 @app.route('/questions')
 def questions():
     #Home page
-    sql = """;"""
-    result = query_db(sql)
-    return str(result)
+    #sql = """;"""
+    #result = query_db(sql)
+    return render_template("layout.html")
 
 
 
