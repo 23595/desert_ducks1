486bff4ced767c2d88ef7697bb0643f8c22f6bbb
23595 <23595@burnside.school.nz>
2026-03-02 10:44:10 +1300

    Successfully goes through questions in order, then directs to scoring page.

--- app.py
+++ app.py
@@ -33,13 +33,15 @@ def home():
 
 @app.route('/setting_select')
 def setting_select():
+    #Presents list of settings to choose from
+    #Random option also available
     sql = 'SELECT COUNT(setting_id) FROM settings'
     result = query_db(sql)
-    result = result[0]
+    result = result[0] #Finds the total number of settings
     settingcount = int(result[0])
-    random_setting = random.randint(1, settingcount)
+    random_setting = random.randint(1, settingcount) #randomly chooses one setting_id
     sql = 'SELECT setting_name, setting_id FROM settings'
-    result = query_db(sql)
+    result = query_db(sql) #Gets a list of all settings with ids
     return render_template("setting_select.html",result=result,random_setting=random_setting)
 
 @app.route('/setting/<int:id>')
@@ -48,40 +50,32 @@ def setting(id):
     WHERE setting_id = ?;"""
     result = query_db(sql, (id,), True)
     #Get the first question ready
-    sql = """SELECT questions.question_id FROM questions
-JOIN questions_bridge ON questions.question_id=questions_bridge.question_id
-WHERE questions_bridge.setting_id = ?;"""
-    questions = query_db(sql, (id,))
-    question_list = []
-    for i in questions:
-        question_list.append(i[0])
-    return render_template("setting_desc.html",result=result,question_list=question_list,id=id,first=question_list[0])
+    return render_template("setting_desc.html",result=result,id=id,first=1)
     
 
 
 @app.route('/questions/<int:id>/<int:questionid>')
 def questions(id,questionid):
-    sql = """SELECT settings.setting_name, questions.question_text, answer_options.answer_text
+    sql = """SELECT COUNT(question_id) 
 FROM questions_bridge
-JOIN settings ON questions_bridge.setting_id=settings.setting_id
-JOIN questions ON questions_bridge.question_id=questions.question_id
-JOIN answer_options ON questions_bridge.question_id=answer_options.question_id
-WHERE questions_bridge.setting_id=?
-AND questions_bridge.question_id=?;"""
-    result = query_db(sql, (id,questionid))
-    first = result[0]
-    sql = """SELECT questions.question_id 
-    FROM questions
-    JOIN questions_bridge 
-    ON questions.question_id=questions_bridge.question_id
-    WHERE questions_bridge.setting_id = """ + str(id) + ';'
-    question_ids = query_db(sql)
-    question_id_list = []
-    for x in question_ids:
-        question_id_list.append(x[0])
-    return render_template("index.html", result=result, question=first[1], id=id, question_id_list=question_id_list)
-
-
+WHERE setting_id = 1;""" #Count the number of questions for the setting
+    question_count = query_db(sql)
+    question_count = question_count[0]
+    question_count = int(question_count[0]) #Convert to int
+    if questionid <= question_count: #Continue if the questionid is valid
+        sql = """SELECT settings.setting_name, questions.question_text, answer_options.answer_text
+        FROM questions_bridge
+        JOIN settings ON questions_bridge.setting_id=settings.setting_id
+        JOIN questions ON questions_bridge.question_id=questions.question_id
+        JOIN answer_options ON questions_bridge.question_id=answer_options.question_id
+        WHERE questions_bridge.setting_id=?
+        AND questions_bridge.question_id=?;"""
+        result = query_db(sql, (id,questionid))
+        first = result[0]
+        return render_template("questions.html", result=result, question=first[1], id=id, nextid=questionid+1)
+    else: #If the questionid is not valid, aka all questions have been asked
+        return render_template("scoring.html")
+    
 
 if __name__ == "__main__":
     app.run(debug=True)
