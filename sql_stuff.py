ecba192fe53ce4f1aa6b846ac9b9adab52e52b4c
23595 <23595@burnside.school.nz>
2026-04-20 14:40:39 +1200

    Adding comments and small edits

--- sql_stuff.py
+++ sql_stuff.py
@@ -2,6 +2,7 @@ import sqlite3
 
 DATABASE = 'ducks.db'
 
+
 def query_db(sql):
     with sqlite3.connect(DATABASE) as db:
         cursor = db.cursor()
@@ -9,32 +10,41 @@ def query_db(sql):
         results = cursor.fetchall()
         return results
 
-#For every setting, go through each question and each answer and add ans_points of ten and ans_explain of the setting, question, answer to the game_logic table
+# For every setting, go through each question and each answer and add
+# ans_points of ten and ans_explain of the setting, question, answer
+# to the game_logic table
 
-if __name__ == "__main__":
+
+def add_pts_ex():
     sql = 'SELECT settings.setting_id, settings.setting_name FROM settings'
     settingid_list = query_db(sql)
-    for settingid in settingid_list:  #For each setting pair
+    for settingid in settingid_list:  # For each setting pair
         settingname = settingid[1]
         settingid = settingid[0]
-        sql = """SELECT questions_bridge.question_id, questions.question_text 
+        sql = """SELECT questions_bridge.question_id, questions.question_text
                 FROM questions_bridge
-                JOIN questions ON questions_bridge.question_id = questions.question_id 
+                JOIN questions ON questions_bridge.question_id = questions.question_id
                 WHERE questions_bridge.setting_id = """ + str(settingid) + ";"
         questionid_list = query_db(sql)
-        for questionid in questionid_list: #For each question
+        for questionid in questionid_list:  # For each question
             questiontext = questionid[1]
             questionid = questionid[0]
             sql = "SELECT option_id, answer_text FROM answer_options WHERE question_id = " + str(questionid) + ";"
             optionid_list = query_db(sql)
-            for optionid in optionid_list: #For each possible answer
+            for optionid in optionid_list: # For each possible answer
                 optiontext = optionid[1]
                 optionid = optionid[0]
-                #sql = """INSERT INTO game_logic (setting_id, question_id, option_id, ans_points, ans_explain)
+                # sql = """INSERT INTO game_logic (setting_id, question_id, option_id, ans_points, ans_explain)
                 #        VALUES (""" + str(settingid) + ", " + str(questionid) + ", " + str(optionid) + ", 10, '"
-                #sql = sql + f"{settingname[:5]} {questiontext[:5]} {optiontext[:5]}';"
+                # sql = sql + f"{settingname[:5]} {questiontext[:5]} {optiontext[:5]}';"
                 sql = f"""INSERT INTO game_logic (setting_id, question_id, option_id, ans_points, ans_explain)
                         VALUES ({settingid}, {questionid}, {optionid}, 10, '{settingname[:5]} {questiontext[:5]} {optiontext[:5]}');"""
                 with sqlite3.connect(DATABASE) as db:
                     cursor = db.cursor()
                     cursor.execute(sql)
+
+
+def add_values():
+    settings_sql = '''INSERT INTO settings (setting_name, setting_desc) VALUES (), (), ();'''
+    questions_sql = '''INSERT INTO questions (question_text) VALUES (), ();'''
+    bridge_sql = '''INSERT INTO questions_bridge (setting_id, question_id) VALUES (), ();'''
