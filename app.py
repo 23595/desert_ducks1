715fefe5fc7171a0373637a05605c5dc5f2a5cb0
23595 <23595@burnside.school.nz>
2026-05-06 10:11:23 +1200

    Attempting to fix javascript displaying the answers for a selected setting and question - not currently working

--- app.py
+++ app.py
@@ -20,13 +20,43 @@ def get_db():
 
 
 def count_questions(setting_id) -> int:
-    sql = """SELECT COUNT(question_id) 
+    sql = """SELECT COUNT(question_id)
             FROM questions_bridge
             WHERE setting_id = """ + str(setting_id) + ";"  # Count the number of questions for the selected setting
     result = query_db(sql)
     result = result[0]
     result = int(result[0])
     return result
+
+
+def get_admin_values():
+    # Get settings and questions
+    sql = """SELECT settings.setting_name, questions.question_text
+    FROM questions_bridge
+    JOIN settings ON questions_bridge.setting_id=settings.setting_id
+    JOIN questions ON questions_bridge.question_id=questions.question_id;"""
+    setting_question = query_db(sql)
+    data_dict = {}
+    for setting, question in setting_question:
+        if setting not in data_dict:
+            data_dict[setting] = []
+        data_dict[setting].append(question)
+    # Get all settings, questions, and answers
+    sql = """SELECT settings.setting_name, questions.question_text, answer_options.answer_text, game_logic.ans_points, game_logic.ans_explain
+        FROM game_logic
+        JOIN settings ON game_logic.setting_id=settings.setting_id
+        JOIN answer_options ON game_logic.option_id=answer_options.option_id
+        JOIN questions ON game_logic.question_id=questions.question_id;"""
+    results = query_db(sql)
+    everything_dict = {}
+    for set, ques, ans_txt, ans_pts, ans_ex in results:
+        if set not in everything_dict:
+            everything_dict[set] = {}
+        if ques not in everything_dict[set]:
+            everything_dict[set][ques] = {}
+        everything_dict[set][ques][ans_txt] = [ans_pts, ans_ex]
+    return data_dict, everything_dict
+
 
 @app.teardown_appcontext
 def close_connection(exception):
@@ -47,22 +77,10 @@ def home():
     # Home page
     return render_template("homepage.html")
 
+
 @app.route('/admin_login')
 def admin_login():
     return render_template("login.html")
-                        
-@app.route('/setting_select')
-def setting_select():
-    # Presents list of settings to choose from
-    # Random option also available
-    sql = 'SELECT COUNT(setting_id) FROM settings'
-    result = query_db(sql)
-    result = result[0]  # Finds the total number of settings
-    settingcount = int(result[0])
-    random_setting = random.randint(1, settingcount) # randomly chooses one setting_id
-    sql = 'SELECT setting_name, setting_id FROM settings'
-    result = query_db(sql)  # Gets a list of all settings with ids
-    return render_template("setting_select.html", result=result, random_setting=random_setting) # Sends the list of settings and id for the random one
 
 @app.route('/setting_list')
 def setting_list():
@@ -72,10 +90,23 @@ def setting_list():
     result = query_db(sql)
     result = result[0]  # Finds the total number of settings
     settingcount = int(result[0])
-    random_setting = random.randint(1, settingcount) # randomly chooses one setting_id
+    random_setting = random.randint(1, settingcount)  # randomly chooses one setting_id
     sql = 'SELECT setting_name, setting_id FROM settings'
     result = query_db(sql)  # Gets a list of all settings with ids
-    return render_template("iframe.html", result=result, random_setting=random_setting) # Sends the list of settings and id for the random one
+    return render_template("iframe.html", result=result, random_setting=random_setting)  # Sends the list of settings and id for the random one
+
+@app.route('/setting_select')
+def setting_select():
+    # Presents list of settings to choose from
+    # Random option also available
+    sql = 'SELECT COUNT(setting_id) FROM settings'
+    result = query_db(sql)
+    result = result[0]  # Finds the total number of settings
+    settingcount = int(result[0])
+    random_setting = random.randint(1, settingcount)  # randomly chooses one setting_id
+    sql = 'SELECT setting_name, setting_id FROM settings'
+    result = query_db(sql)  # Gets a list of all settings with ids
+    return render_template("setting_select.html", result=result, random_setting=random_setting)  # Sends the list of settings and id for the random one
 
 
 @app.route('/setting/<int:id>')
@@ -95,12 +126,13 @@ def setting(id):
     result = query_db(sql, (id,), True)
     return render_template("setting_desc.html", result=result, id=id, first=0)
 
+
 @app.route('/questions/<int:id>/<int:on_question>', methods=['GET', 'POST'])
 def questions(id, on_question):  # id is the id of the setting. on_question is the location in the list of question ids of the current question id
     if request.method == 'POST':
         prev_id = questions_list[on_question - 1]
         answer = request.form.get('choose_ans')
-        sql = f"""SELECT game_logic.ans_explain, game_logic.ans_points 
+        sql = f"""SELECT game_logic.ans_explain, game_logic.ans_points
                 FROM game_logic
                 JOIN answer_options ON game_logic.option_id=answer_options.option_id
                 WHERE game_logic.setting_id = {id}
@@ -110,7 +142,7 @@ def questions(id, on_question):  # id is the id of the setting. on_question is t
         result = result[0]
         answers_list.append(result[0])
         answers_list.append(result[1])
-    question_count = count_questions(id) # returns int
+    question_count = count_questions(id)  # returns int
     if int(on_question) < question_count:  # Continue if the questionid is valid
         current_id = questions_list[on_question]
         sql = f"""SELECT settings.setting_name, questions.question_text, answer_options.answer_text
@@ -121,7 +153,7 @@ def questions(id, on_question):  # id is the id of the setting. on_question is t
         WHERE questions_bridge.setting_id={id}
         AND questions_bridge.question_id={current_id};"""  # Get the setting name, question text, and answer options
         result = query_db(sql)
-        first = result[0] 
+        first = result[0]
         if len(answers_list) > 2 * (on_question):
             del answers_list[-1]
             del answers_list[-1]
@@ -136,16 +168,17 @@ def questions(id, on_question):  # id is the id of the setting. on_question is t
                 pass
             else:
                 total += int(answers_list[i])
-        #Get setting name
+        # Get setting name
         sql = f'SELECT settings.setting_name FROM settings WHERE setting_id={id};'
         setting_name = query_db(sql)
         return render_template("scoring.html", answers_list=answers_list, total=total, setting_name=setting_name[0][0])
-    
+
+
 @app.route('/new_user', methods=['GET', 'POST'])
 def new_user():
     error_message = ''
     # Get everything needed for the admin page
-    data_dict, everything_dict, usernames = get_admin_values()
+    data_dict, everything_dict = get_admin_values()
     # Continue
     if request.method == 'POST':
         password = request.form['password']
@@ -159,10 +192,10 @@ def new_user():
             # Returns an error message if the username already exists
         elif len(username) < 4:
             error_message = 'Username must be at least 4 characters'
-             # Returns an error message if the username is too short
+            # Returns an error message if the username is too short
         elif len(password) < 4:
             error_message = 'Password must be at least 4 characters'
-             # Returns an error message if the password is too short
+            # Returns an error message if the password is too short
         elif check_password != password:
             error_message = 'Passwords did not match. Ensure that Password and Confirm Password are the same.'
             # Returns an error message if the passwords do not match
@@ -173,50 +206,15 @@ def new_user():
                 sql = """INSERT INTO admin_login (username, encrypted_pw, authority_lvl)
                 VALUES ('""" + username + "', '" + hashed_pw + "', 1);"
                 cursor.execute(sql)
-            error_message = "New admin '" + username +"' created successfully"
-    return render_template("admin.html", setting_names=data_dict.keys(), setting_data=json.dumps(data_dict), everything=json.dumps(everything_dict), usernames=usernames, error_message=error_message)
-    
-
-def get_admin_values():
-    # Get settings and questions
-    sql = """SELECT settings.setting_name, questions.question_text
-    FROM questions_bridge
-    JOIN settings ON questions_bridge.setting_id=settings.setting_id
-    JOIN questions ON questions_bridge.question_id=questions.question_id;"""
-    setting_question = query_db(sql)
-    data_dict = {}
-    for setting, question in setting_question:
-        if setting not in data_dict:
-            data_dict[setting] = []
-        data_dict[setting].append(question)
-    # Get all settings, questions, and answers
-    sql = """SELECT settings.setting_name, questions.question_text, answer_options.answer_text, game_logic.ans_points, game_logic.ans_explain
-        FROM game_logic
-        JOIN settings ON game_logic.setting_id=settings.setting_id
-        JOIN answer_options ON game_logic.option_id=answer_options.option_id
-        JOIN questions ON game_logic.question_id=questions.question_id;"""
-    results = query_db(sql)
-    everything_dict = {}
-    for set, ques, ans_txt, ans_pts, ans_ex in results:
-        if set not in everything_dict:
-            everything_dict[set] = {}
-        if ques not in everything_dict[set]:
-            everything_dict[set][ques] = {}
-        everything_dict[set][ques][ans_txt] = [ans_pts, ans_ex]
-    # Get list of existing usernames
-    sql = """SELECT username FROM admin_login"""
-    results = query_db(sql)
-    usernames = []
-    for result in results:
-        usernames.append(result[0])
-    return data_dict, everything_dict, usernames
+            error_message = "New admin '" + username + "' created successfully"
+    return render_template("admin.html", setting_names=data_dict.keys(), setting_data=json.dumps(data_dict), everything=json.dumps(everything_dict), error_message=error_message)
 
 
 @app.route('/remove_user', methods=['GET', 'POST'])
 def remove_user():
     error_message = ''
     # Get everything needed for the admin page
-    data_dict, everything_dict, usernames = get_admin_values()
+    data_dict, everything_dict = get_admin_values()
     # Continue
     if request.method == 'POST':
         username = request.form['username']
@@ -229,46 +227,12 @@ def remove_user():
                 sql = """DELETE FROM admin_login
                 WHERE username = '""" + username + "';"
                 cursor.execute(sql)
-            error_message = "Admin '" + username +"' has been deleted"
+            error_message = "Admin '" + username + "' has been deleted"
         else:
             error_message = 'Username not found. Check spelling and capitalisation.'
             # Returns an error message if it cannot find the username
-    return render_template("admin.html", setting_names=data_dict.keys(), setting_data=json.dumps(data_dict), everything=json.dumps(everything_dict), usernames=usernames, error_message=error_message)
-    
+    return render_template("admin.html", setting_names=data_dict.keys(), setting_data=json.dumps(data_dict), everything=json.dumps(everything_dict), error_message=error_message)
 
-def get_admin_values():
-    # Get settings and questions
-    sql = """SELECT settings.setting_name, questions.question_text
-    FROM questions_bridge
-    JOIN settings ON questions_bridge.setting_id=settings.setting_id
-    JOIN questions ON questions_bridge.question_id=questions.question_id;"""
-    setting_question = query_db(sql)
-    data_dict = {}
-    for setting, question in setting_question:
-        if setting not in data_dict:
-            data_dict[setting] = []
-        data_dict[setting].append(question)
-    # Get all settings, questions, and answers
-    sql = """SELECT settings.setting_name, questions.question_text, answer_options.answer_text, game_logic.ans_points, game_logic.ans_explain
-        FROM game_logic
-        JOIN settings ON game_logic.setting_id=settings.setting_id
-        JOIN answer_options ON game_logic.option_id=answer_options.option_id
-        JOIN questions ON game_logic.question_id=questions.question_id;"""
-    results = query_db(sql)
-    everything_dict = {}
-    for set, ques, ans_txt, ans_pts, ans_ex in results:
-        if set not in everything_dict:
-            everything_dict[set] = {}
-        if ques not in everything_dict[set]:
-            everything_dict[set][ques] = {}
-        everything_dict[set][ques][ans_txt] = [ans_pts, ans_ex]
-    # Get list of existing usernames
-    sql = """SELECT username FROM admin_login"""
-    results = query_db(sql)
-    usernames = []
-    for result in results:
-        usernames.append(result[0])
-    return data_dict, everything_dict, usernames
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -284,9 +248,9 @@ def login():
             result = results[0]
             if check_password_hash(result[1], password):
                 sql = """SELECT settings.setting_name, questions.question_text
-FROM questions_bridge
-JOIN settings ON questions_bridge.setting_id=settings.setting_id
-JOIN questions ON questions_bridge.question_id=questions.question_id;"""
+                        FROM questions_bridge
+                        JOIN settings ON questions_bridge.setting_id=settings.setting_id
+                        JOIN questions ON questions_bridge.question_id=questions.question_id;"""
                 setting_question = query_db(sql)
                 data_dict = {}
                 for setting, question in setting_question:
@@ -294,7 +258,9 @@ JOIN questions ON questions_bridge.question_id=questions.question_id;"""
                         data_dict[setting] = []
                     data_dict[setting].append(question)
                 # Get all settings, questions, and answers
-                sql = """SELECT settings.setting_name, questions.question_text, answer_options.answer_text, game_logic.ans_points, game_logic.ans_explain
+                sql = """SELECT settings.setting_name, questions.question_text,
+                    answer_options.answer_text, game_logic.ans_points,
+                    game_logic.ans_explain
                     FROM game_logic
                     JOIN settings ON game_logic.setting_id=settings.setting_id
                     JOIN answer_options ON game_logic.option_id=answer_options.option_id
@@ -307,20 +273,14 @@ JOIN questions ON questions_bridge.question_id=questions.question_id;"""
                     if ques not in everything_dict[set]:
                         everything_dict[set][ques] = {}
                     everything_dict[set][ques][ans_txt] = [ans_pts, ans_ex]
-                # Get list of existing usernames
-                sql = """SELECT username FROM admin_login"""
-                results = query_db(sql)
-                usernames = []
-                for result in results:
-                    usernames.append(result[0])
-                return render_template("admin.html", setting_names=data_dict.keys(), setting_data=json.dumps(data_dict), everything=json.dumps(everything_dict), error_message='', usernames=usernames)  # go to admin page
+                return render_template("admin.html", setting_names=data_dict.keys(), setting_data=json.dumps(data_dict), everything=json.dumps(everything_dict), error_message='')  # go to admin page
             else:
                 return render_template("login.html", message='Incorrect Password')  # Gives error message
         else:
             return render_template("login.html", message='Username not found')  # Gives error message
     else:
         return render_template("login.html")
-    
+
 
 if __name__ == "__main__":
     app.run(debug=True)
